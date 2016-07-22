8949adb8c912c1058aadc336a7b7e5236c6a6257
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-22 09:01:27 -0300

    Incluindo projeto de testes funcionais para iOS

---
+++ Mambo.FunctionalTests.Android/Mambo.FunctionalTests.iOS/AppDelegateTest.cs
@@ -0,0 +1,31 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Mambo.FunctionalTests.iOS
+{
+	/// <summary>
+	/// App delegate test.
+	/// </summary>
+	[Register("AppDelegateTest")]
+	public partial class AppDelegateTest : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		/// <summary>
+		/// Finisheds the launching.
+		/// </summary>
+		/// <returns>The launching.</returns>
+		/// <param name="app">App.</param>
+		/// <param name="options">Options.</param>
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init();
+
+			var nunit = new NUnit.Runner.App {
+				AutoRun = true
+			};
+			//nunit.AddTestAssembly(typeof(FunctionalTests).Assembly);
+			LoadApplication(nunit);
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
---
+++ Mambo.FunctionalTests.Android/Mambo.FunctionalTests.iOS/Main.cs
@@ -0,0 +1,19 @@
+﻿using UIKit;
+
+namespace Mambo.FunctionalTests.iOS
+{
+	/// <summary>
+	/// Application.
+	/// </summary>
+	public class Application
+	{
+		/// <summary>
+		/// The entry point of the program, where the program control starts and ends.
+		/// </summary>
+		/// <param name="args">The command-line arguments.</param>
+		static void Main(string[] args)
+		{
+			UIApplication.Main(args, null, "AppDelegateTest");
+		}
+	}
+}
