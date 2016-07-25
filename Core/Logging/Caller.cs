f952b4ee9eca55ce66bae2503843121bf29d0058
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-25 16:09:29 -0300

    Caller: Incluindo classe estática para log

---
+++ Core/Logging/Caller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Mambo.Core.Logging
+{
+	/// <summary>
+	/// Caller.
+	/// </summary>
+	public static class Caller
+	{
+		/// <summary>
+		/// Gets the info.
+		/// </summary>
+		/// <returns>The info.</returns>
+		/// <param name="filePath">File path.</param>
+		/// <param name="memberName">Member name.</param>
+		/// <param name="lineNumber">Line number.</param>
+		public static string GetInfo([CallerFilePath] string filePath = "", [CallerMemberName] string memberName = "", [CallerLineNumber] int lineNumber = 0)
+		{
+			var fileName = filePath.Substring(filePath.LastIndexOf("/", StringComparison.CurrentCulture) + 1);
+			var className = fileName.Replace(".cs", "");
+
+			return $"{className}.{memberName}:{lineNumber}";
+		}
+	}
+}
