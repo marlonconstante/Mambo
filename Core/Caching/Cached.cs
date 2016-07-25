20937425589b7f8ae7a0446b129dcf8c9eebc825
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-25 16:05:53 -0300

    Incluindo classe estática responsável pelo Cache

---
+++ Core/Caching/Cached.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Akavache;
+
+namespace Mambo.Core.Caching
+{
+	/// <summary>
+	/// Cached.
+	/// </summary>
+	public static class Cached
+	{
+		/// <summary>
+		/// Gets the value.
+		/// </summary>
+		/// <returns>The value.</returns>
+		/// <param name="cacheKey">Cache key.</param>
+		/// <param name="fetchFunction">Fetch function.</param>
+		/// <param name="cacheValidityInMinutes">Cache validity in minutes.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static async Task<T> GetValue<T>(string cacheKey, Func<Task<T>> fetchFunction, double cacheValidityInMinutes = 5d)
+		{
+			return await BlobCache.LocalMachine.GetAndFetchLatest(cacheKey, fetchFunction, offset => {
+				var elapsed = DateTimeOffset.Now - offset;
+				return elapsed > TimeSpan.FromMinutes(cacheValidityInMinutes);
+			}).FirstOrDefaultAsync();
+		}
+	}
+}
