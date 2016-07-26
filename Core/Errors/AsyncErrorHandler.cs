8c4e9559d2388d495b4df01681d1a04be5ee3878
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-26 11:29:25 -0300

    Incluindo manipulador de erros assíncronos

---
+++ Core/Errors/AsyncErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Mambo.Core.Errors
+{
+	/// <summary>
+	/// Async error handler.
+	/// </summary>
+	public static class AsyncErrorHandler
+	{
+		/// <summary>
+		/// Handles the exception.
+		/// </summary>
+		/// <returns>The exception.</returns>
+		/// <param name="exception">Exception.</param>
+		public static void HandleException(Exception exception)
+		{
+			Debug.WriteLine(exception.Message);
+		}
+	}
+}
