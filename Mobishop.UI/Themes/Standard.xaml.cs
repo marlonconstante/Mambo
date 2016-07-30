5df560c66a25529551395dd5683f080a39b0fb8f
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-30 20:42:32 -0300

    Incluindo tema “Standard”

---
+++ Mobishop.UI/Themes/Standard.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Mobishop.UI.Themes
+{
+	/// <summary>
+	/// Standard.
+	/// </summary>
+	public partial class Standard
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Themes.Standard"/> class.
+		/// </summary>
+		public Standard()
+		{
+			InitializeComponent();
+		}
+	}
+}
