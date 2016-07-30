fa4f2aa14ce686fe318c2f05e020d7c411c1d6ed
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-29 21:43:46 -0300

    Incluindo tela inicial do app

---
+++ Pages/HomePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Mambo.Pages
+{
+	/// <summary>
+	/// Home page.
+	/// </summary>
+	public partial class HomePage : ContentPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.Pages.HomePage"/> class.
+		/// </summary>
+		public HomePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
