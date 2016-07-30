0ceb8e16a2f92f3aa7a52b40e0e4ccf2328d7a98
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-29 21:48:14 -0300

    Page model da tela inicial

---
+++ PageModels/HomePageModel.cs
@@ -0,0 +1,19 @@
+﻿using FreshMvvm;
+using PropertyChanged;
+
+namespace Mambo.PageModels
+{
+	/// <summary>
+	/// Home page model.
+	/// </summary>
+	[ImplementPropertyChanged]
+	public class HomePageModel : FreshBasePageModel
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.PageModels.HomePageModel"/> class.
+		/// </summary>
+		public HomePageModel()
+		{
+		}
+	}
+}
