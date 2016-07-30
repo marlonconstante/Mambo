acf3fffa9c8d1cf2aa90215f29d1a14164a787ea
Marlon Santos Constante <marlon.constante@gmail.com>
2016-07-30 15:59:07 -0300

    Incluindo componente “Mobishop.UI.Controls.PriceLabel”

---
+++ Mobishop.UI/Controls/PriceLabel.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Mobishop.UI.Controls
+{
+	/// <summary>
+	/// Price label.
+	/// </summary>
+	public partial class PriceLabel : ContentView
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Controls.PriceLabel"/> class.
+		/// </summary>
+		public PriceLabel()
+		{
+			InitializeComponent();
+		}
+	}
+}
