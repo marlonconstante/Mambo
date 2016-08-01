a53483a7940c32c46139bd7f5b543677fe5f1a8e
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-01 11:54:57 -0300

    Incluindo modelo “PriceLabelViewModel”

---
+++ Mobishop.UI/ViewModels/PriceLabelViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using PropertyChanged;
+
+namespace Mobishop.UI.ViewModels
+{
+	/// <summary>
+	/// Price label view model.
+	/// </summary>
+	[ImplementPropertyChanged]
+	public class PriceLabelViewModel
+	{
+		/// <summary>
+		/// Gets the symbol.
+		/// </summary>
+		/// <value>The symbol.</value>
+		public string Symbol {
+			get {
+				return CultureInfo.CurrentCulture.NumberFormat.CurrencySymbol;
+			}
+		}
+
+		/// <summary>
+		/// Gets the separator.
+		/// </summary>
+		/// <value>The separator.</value>
+		public string Separator {
+			get {
+				return CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+			}
+		}
+
+		/// <summary>
+		/// Gets the integer amount.
+		/// </summary>
+		/// <value>The integer amount.</value>
+		public string IntegerAmount {
+			get {
+				return Amount.ToString("F2").Split(Convert.ToChar(Separator)).First();
+			}
+		}
+
+		/// <summary>
+		/// Gets the decimal amount.
+		/// </summary>
+		/// <value>The decimal amount.</value>
+		public string DecimalAmount {
+			get {
+				return Amount.ToString("F2").Split(Convert.ToChar(Separator)).Last();
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the amount.
+		/// </summary>
+		/// <value>The amount.</value>
+		public double Amount {
+			get;
+			set;
+		}
+	}
+}
