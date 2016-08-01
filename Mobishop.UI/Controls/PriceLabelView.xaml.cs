b8d309fde30f340a32546251203706cd0c8a3cd4
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-01 11:52:46 -0300

    Construção do componente “PriceLabelView”

---
+++ Mobishop.UI/Controls/PriceLabelView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Mobishop.UI.ViewModels;
+using Xamarin.Forms;
+
+namespace Mobishop.UI.Controls
+{
+	/// <summary>
+	/// Price label view.
+	/// </summary>
+	public partial class PriceLabelView : ContentView
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Controls.PriceLabelView"/> class.
+		/// </summary>
+		public PriceLabelView()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Gets or sets the amount.
+		/// </summary>
+		/// <value>The amount.</value>
+		public double Amount {
+			get {
+				return (BindingContext as PriceLabelViewModel).Amount;
+			}
+			set {
+				(BindingContext as PriceLabelViewModel).Amount = value;
+			}
+		}
+	}
+}
