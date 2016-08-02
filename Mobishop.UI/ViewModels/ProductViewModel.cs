b2ca7f5733fc7792f105a230bfc019703caea902
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-02 08:52:42 -0300

    Incluindo ViewModel “ProductViewModel”

---
+++ Mobishop.UI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using PropertyChanged;
+
+namespace Mobishop.UI.ViewModels
+{
+	/// <summary>
+	/// Product view model.
+	/// </summary>
+	[ImplementPropertyChanged]
+	public class ProductViewModel
+	{
+
+	}
+}
