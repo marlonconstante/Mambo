1a96c760bba036fdb2d6608df479a8be5dea5951
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-02 08:49:02 -0300

    Incluindo controle “ProductView”

---
+++ Mobishop.UI/Controls/ProductView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Mobishop.UI.Controls
+{
+	/// <summary>
+	/// Product view.
+	/// </summary>
+	public partial class ProductView : ContentView
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Controls.ProductView"/> class.
+		/// </summary>
+		public ProductView()
+		{
+			InitializeComponent();
+		}
+	}
+}
