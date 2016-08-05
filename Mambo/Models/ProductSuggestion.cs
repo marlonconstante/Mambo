9bc15fb80ac0d742e07892d9cb8d832dea427d4a
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-05 11:02:04 -0300

    Incluindo domínio de sugestão de produtos

---
+++ Mambo/Models/ProductSuggestion.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace Mambo.Models
+{
+	/// <summary>
+	/// Product suggestion.
+	/// </summary>
+	public class ProductSuggestion
+	{
+		/// <summary>
+		/// Gets or sets the identifier.
+		/// </summary>
+		/// <value>The identifier.</value>
+		[JsonProperty("id")]
+		public long Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the description.
+		/// </summary>
+		/// <value>The description.</value>
+		[JsonProperty("descricao")]
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Gets or sets the previous price.
+		/// </summary>
+		/// <value>The previous price.</value>
+		[JsonProperty("de")]
+		public double PreviousPrice { get; set; }
+
+		/// <summary>
+		/// Gets or sets the current price.
+		/// </summary>
+		/// <value>The current price.</value>
+		[JsonProperty("preco")]
+		public double CurrentPrice { get; set; }
+
+		/// <summary>
+		/// Gets or sets the image URL.
+		/// </summary>
+		/// <value>The image URL.</value>
+		[JsonProperty("imagem")]
+		public string ImageUrl { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="T:Mambo.Models.ProductSuggestion"/> is available.
+		/// </summary>
+		/// <value><c>true</c> if is available; otherwise, <c>false</c>.</value>
+		[JsonProperty("disponivel")]
+		public bool IsAvailable { get; set; }
+	}
+}
---
+++ Mambo/Models/Suggestion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Mambo.Models
+{
+	/// <summary>
+	/// Suggestion.
+	/// </summary>
+	public class Suggestion
+	{
+		/// <summary>
+		/// Gets or sets the queries.
+		/// </summary>
+		/// <value>The queries.</value>
+		[JsonProperty("sugestoes")]
+		public IList<QuerySuggestion> Queries { get; set; }
+
+		/// <summary>
+		/// Gets or sets the products.
+		/// </summary>
+		/// <value>The products.</value>
+		[JsonProperty("produtos")]
+		public IList<ProductSuggestion> Products { get; set; }
+	}
+}
