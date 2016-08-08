53271729d57e7bf0bcbe24a1e0edefadf03649eb
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-08 15:56:29 -0300

    Incluindo página “ProductSearchPage”

---
+++ Mambo/Pages/ProductSearchPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Mambo.Pages
+{
+	/// <summary>
+	/// Product search page.
+	/// </summary>
+	public partial class ProductSearchPage : ContentPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.Pages.ProductSearchPage"/> class.
+		/// </summary>
+		public ProductSearchPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
