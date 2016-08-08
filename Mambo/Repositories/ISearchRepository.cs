939643d86d0c3fb3a798e419d4913c6e91b7f12c
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-08 08:46:51 -0300

    Incluindo interface de pesquisa

---
+++ Mambo/Repositories/ISearchRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mambo.Models;
+using Mambo.Neemu;
+using Refit;
+
+namespace Mambo.Repositories
+{
+	/// <summary>
+	/// Search repository.
+	/// </summary>
+	public interface ISearchRepository : INeemuRepository
+	{
+		/// <summary>
+		/// Finds the suggestions.
+		/// </summary>
+		/// <returns>The suggestions.</returns>
+		/// <param name="query">Query.</param>
+		/// <param name="size">Size.</param>
+		[Get("/autocomplete/search?q={query}&type=1&numsugestoes={size}&numprods={size}")]
+		Task<IList<Suggestion>> FindSuggestions(string query, int size = 5);
+	}
+}
