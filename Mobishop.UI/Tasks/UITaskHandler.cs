9e6f958841e6a0ca55a8781f4644dc4260250610
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-11 17:11:09 -0300

    Incluindo classe estática “UITaskHandler”

---
+++ Mobishop.UI/Tasks/UITaskHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobishop.UI.Tasks
+{
+	/// <summary>
+	/// UI Task handler.
+	/// </summary>
+	public static class UITaskHandler
+	{
+		/// <summary>
+		/// Execute the specified action.
+		/// </summary>
+		/// <param name="action">Action.</param>
+		public static async Task Execute(Func<Task> action)
+		{
+			try
+			{
+				await action.Invoke();
+			}
+			catch (Exception exception)
+			{
+				await Application.Current.MainPage.DisplayAlert("Ops...", exception.Message, "OK");
+			}
+		}
+	}
+}
