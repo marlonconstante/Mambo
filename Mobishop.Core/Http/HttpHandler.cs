f7c8342cb7bc636426840be73cb49dad3e702776
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-12 09:46:14 -0300

    HttpHandler: Suporte para CancellationToken

--- Mobishop.Core/Http/HttpHandler.cs
+++ Mobishop.Core/Http/HttpHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using Plugin.Connectivity;
 using Polly;
@@ -12,18 +13,19 @@ namespace Mobishop.Core.Http
 	public static class HttpHandler
 	{
 		/// <summary>
-		/// Execute the specified remoteFunction and attempts.
+		/// Execute the specified remoteFunction, cancellationToken and attempts.
 		/// </summary>
 		/// <param name="remoteFunction">Remote function.</param>
+		/// <param name="cancellationToken">Cancellation token.</param>
 		/// <param name="attempts">Attempts.</param>
 		/// <typeparam name="TResult">The 1st type parameter.</typeparam>
-		public static Task<TResult> Execute<TResult>(Func<Task<TResult>> remoteFunction, int attempts = 5)
+		public static Task<TResult> Execute<TResult>(Func<CancellationToken, Task<TResult>> remoteFunction, CancellationToken cancellationToken = default(CancellationToken), int attempts = 5)
 		{
 			if (CrossConnectivity.Current.IsConnected)
 			{
 				return Policy.Handle<WebException>()
 							 .WaitAndRetryAsync(attempts, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)))
-							 .ExecuteAsync(remoteFunction);
+							 .ExecuteAsync(remoteFunction, cancellationToken);
 			}
 
 			return Task.FromResult(default(TResult));
