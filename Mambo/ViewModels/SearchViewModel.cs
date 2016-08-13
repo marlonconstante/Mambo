4d3a2fa4ad5af784323be12c8264f04c0267dd95
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-13 07:45:07 -0300

    ViewModel de pesquisa

---
+++ Mambo/ViewModels/SearchViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Mambo.Models;
+using PropertyChanged;
+
+namespace Mambo.ViewModels
+{
+	/// <summary>
+	/// Search view model.
+	/// </summary>
+	[ImplementPropertyChanged]
+	public class SearchViewModel
+	{
+		/// <summary>
+		/// Gets or sets the suggestion.
+		/// </summary>
+		/// <value>The suggestion.</value>
+		public Suggestion Suggestion {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the product.
+		/// </summary>
+		/// <value>The product.</value>
+		public ProductSuggestion Product {
+			get;
+			set;
+		}
+	}
+}
