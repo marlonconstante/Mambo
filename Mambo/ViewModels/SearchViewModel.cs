1a0b9e3470fe0942504cc9bd6c53ee215406055d
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-13 08:42:30 -0300

    SearchViewModel: Construtores

--- Mambo/ViewModels/SearchViewModel.cs
+++ Mambo/ViewModels/SearchViewModel.cs
@@ -10,6 +10,31 @@ namespace Mambo.ViewModels
 	public class SearchViewModel
 	{
 		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.ViewModels.SearchViewModel"/> class.
+		/// </summary>
+		public SearchViewModel()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.ViewModels.SearchViewModel"/> class.
+		/// </summary>
+		/// <param name="suggestion">Suggestion.</param>
+		public SearchViewModel(Suggestion suggestion)
+		{
+			Suggestion = suggestion;
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.ViewModels.SearchViewModel"/> class.
+		/// </summary>
+		/// <param name="product">Product.</param>
+		public SearchViewModel(ProductSuggestion product)
+		{
+			Product = product;
+		}
+
+		/// <summary>
 		/// Gets or sets the suggestion.
 		/// </summary>
 		/// <value>The suggestion.</value>
