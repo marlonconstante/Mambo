43e2c21ccb31125d9f1481ffdf4ff3c297a42e40
Marlon Santos Constante <marlon.constante@gmail.com>
2016-08-31 17:41:43 -0300

    Renomeando tema “Standard” para “StandardTheme”

---
+++ Mobishop.UI/Themes/StandardTheme.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Mobishop.UI.Themes
+{
+	/// <summary>
+	/// Standard theme.
+	/// </summary>
+	public partial class StandardTheme
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Themes.StandardTheme"/> class.
+		/// </summary>
+		public StandardTheme()
+		{
+			InitializeComponent();
+		}
+	}
+}
