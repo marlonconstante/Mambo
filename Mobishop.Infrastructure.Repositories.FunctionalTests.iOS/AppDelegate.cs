e2fbe03de2cd585956ebb10ad1392fb61d8af03d
Marlon Santos Constante <marlon.constante@gmail.com>
2016-09-01 09:13:20 -0300

    Documentação + Identação

--- Mobishop.Infrastructure.Repositories.FunctionalTests.iOS/AppDelegate.cs
+++ Mobishop.Infrastructure.Repositories.FunctionalTests.iOS/AppDelegate.cs
@@ -1,37 +1,46 @@
-﻿using Akavache;
-using Foundation;
+﻿using Foundation;
 using Mobishop.Infrastructure.Repositories.Commons.Caching;
 using Mobishop.Infrastructure.Repositories.FunctionalTests.Vtex.Products;
 using UIKit;
 
 namespace Mobishop.Infrastructure.Repositories.FunctionalTests.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the
-    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
-    [Register("AppDelegate")]
-    public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-    {
-        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
-        {
-            global::Xamarin.Forms.Forms.Init();
+	/// <summary>
+	/// App delegate.
+	/// </summary>
+	[Register("AppDelegate")]
+	public class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		/// <summary>
+		/// Finisheds the launching.
+		/// </summary>
+		/// <returns><c>true</c>, if launching was finisheded, <c>false</c> otherwise.</returns>
+		/// <param name="app">App.</param>
+		/// <param name="options">Options.</param>
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init();
 
-            Cache.Initialize("MobiTests");
+			Cache.Initialize("MobiTests");
 
-            var nunit = new NUnit.Runner.App
-            {
-                AutoRun = false
-            };
-            nunit.AddTestAssembly(typeof(VtexProductRepositoryTest).Assembly);
-            LoadApplication(nunit);
+			var nunit = new NUnit.Runner.App {
+				AutoRun = false
+			};
+			nunit.AddTestAssembly(typeof(VtexProductRepositoryTest).Assembly);
+			LoadApplication(nunit);
 
-            return base.FinishedLaunching(application, launchOptions);
-        }
+			return base.FinishedLaunching(app, options);
+		}
 
-        public override void WillTerminate(UIApplication uiApplication)
-        {
-            base.WillTerminate(uiApplication);
-            Cache.Shutdown();
-        }
-    }
+		/// <summary>
+		/// Wills the terminate.
+		/// </summary>
+		/// <param name="app">App.</param>
+		public override void WillTerminate(UIApplication app)
+		{
+			base.WillTerminate(app);
+
+			Cache.Shutdown();
+		}
+	}
 }
-
