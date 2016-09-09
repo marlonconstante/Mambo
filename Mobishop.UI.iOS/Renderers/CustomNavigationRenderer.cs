c512d0e914f07386206781a7b71c5dfa7d164cde
Marlon Santos Constante <marlon.constante@gmail.com>
2016-09-09 14:21:58 -0300

    Incluindo controle “CustomNavigationPage”

---
+++ Mobishop.UI.iOS/Renderers/CustomNavigationRenderer.cs
@@ -0,0 +1,21 @@
+﻿using FormsPlugin.Iconize.iOS;
+using Mobishop.UI.Controls;
+using Mobishop.UI.iOS.Renderers;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(CustomNavigationPage), typeof(CustomNavigationRenderer))]
+namespace Mobishop.UI.iOS.Renderers
+{
+	/// <summary>
+	/// Custom navigation renderer.
+	/// </summary>
+	public class CustomNavigationRenderer : IconNavigationRenderer
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.iOS.Renderers.CustomNavigationRenderer"/> class.
+		/// </summary>
+		public CustomNavigationRenderer()
+		{
+		}
+	}
+}
---
+++ Mobishop.UI/Controls/CustomNavigationPage.cs
@@ -0,0 +1,20 @@
+﻿using FormsPlugin.Iconize;
+using Xamarin.Forms;
+
+namespace Mobishop.UI.Controls
+{
+	/// <summary>
+	/// Custom navigation page.
+	/// </summary>
+	public class CustomNavigationPage : IconNavigationPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Controls.CustomNavigationPage"/> class.
+		/// </summary>
+		/// <param name="page">Page.</param>
+		public CustomNavigationPage(Page page) : base(page)
+		{
+			Style = (Style) Application.Current.Resources["navigationBarStyle"];
+		}
+	}
+}
