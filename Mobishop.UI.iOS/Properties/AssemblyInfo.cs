71c4d98c02559c87eec5930e0db1bf936595b33b
Marlon Santos Constante <marlon.constante@gmail.com>
2016-09-09 11:12:42 -0300

    Incluindo projetos “Mobishop.UI.Android” e “Mobishop.UI.iOS”

--- Mobishop.UI.iOS/Properties/AssemblyInfo.cs
+++ Mobishop.UI.iOS/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Mobishop.Core")]
+[assembly: AssemblyTitle("Mobishop.UI.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -24,3 +24,4 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
+
