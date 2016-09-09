6ccab15cf7fa1dd158ea40c94b1ec41a250f128d
Marlon Santos Constante <marlon.constante@gmail.com>
2016-09-09 09:51:45 -0300

    Android: Mambo.MainApplication

---
+++ Android/MainApplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace Mambo.Android
+{
+	/// <summary>
+	/// Main application.
+	/// </summary>
+	[Application(Name = "Mambo.MainApplication")]
+	public class MainApplication : Application
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.Android.MainApplication"/> class.
+		/// </summary>
+		/// <param name="handle">Handle.</param>
+		/// <param name="ownership">Ownership.</param>
+		public MainApplication(IntPtr handle, JniHandleOwnership ownership) : base(handle, ownership)
+		{
+		}
+
+		/// <summary>
+		/// Ons the create.
+		/// </summary>
+		public override void OnCreate()
+		{
+			base.OnCreate();
+		}
+	}
+}
