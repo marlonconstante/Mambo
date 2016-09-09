a5e314b9dbe824131713483c5df62e4732ec78ef
Marlon Santos Constante <marlon.constante@gmail.com>
2016-09-09 15:01:09 -0300

    Incluindo controle “CustomSearchBar”

---
+++ Mobishop.UI/Controls/CustomSearchBar.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Mobishop.UI.Controls
+{
+	/// <summary>
+	/// Custom search bar.
+	/// </summary>
+	public class CustomSearchBar : SearchBar
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Controls.CustomSearchBar"/> class.
+		/// </summary>
+		public CustomSearchBar()
+		{
+			Style = (Style) Application.Current.Resources["searchBarStyle"];
+		}
+	}
+}
