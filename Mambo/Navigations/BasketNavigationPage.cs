82d20c3d9e9522f28f7a71bb1c1713d5dad60a65
Marlon Santos Constante <marlon.constante@gmail.com>
2016-09-12 16:00:12 -0300

    Incluindo fluxo de navegação da cesta

---
+++ Mambo/Navigations/BasketNavigationPage.cs
@@ -0,0 +1,27 @@
+﻿using FormsPlugin.Iconize;
+using Mobishop.UI.Controls;
+using Xamarin.Forms;
+
+namespace Mambo.Navigations
+{
+	/// <summary>
+	/// Basket navigation page.
+	/// </summary>
+	public class BasketNavigationPage : CustomNavigationPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mambo.Navigations.BasketNavigationPage"/> class.
+		/// </summary>
+		/// <param name="page">Page.</param>
+		public BasketNavigationPage(Page page) : base(page)
+		{
+			ToolbarItems.Add(new IconToolbarItem {
+				Icon = "md-search"
+			});
+
+			ToolbarItems.Add(new IconToolbarItem {
+				Icon = "md-shopping-cart"
+			});
+		}
+	}
+}
