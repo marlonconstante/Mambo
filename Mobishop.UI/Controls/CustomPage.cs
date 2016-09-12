8491dd14e1fb3f33caf175825c06a616c4a215b0
Marlon Santos Constante <marlon.constante@gmail.com>
2016-09-12 10:50:29 -0300

    Incluindo “CustomPage”

---
+++ Mobishop.UI/Controls/CustomPage.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace Mobishop.UI.Controls
+{
+	/// <summary>
+	/// Custom page.
+	/// </summary>
+	public class CustomPage : ContentPage
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Mobishop.UI.Controls.CustomPage"/> class.
+		/// </summary>
+		public CustomPage()
+		{
+			Style = (Style) Application.Current.Resources["pageStyle"];
+
+			NavigationPage.SetTitleIcon(this, (string) Application.Current.Resources["titleIconImageName"]);
+		}
+	}
+}
